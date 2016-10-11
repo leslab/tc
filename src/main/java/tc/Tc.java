30f62aa4ee5a5529e962c8e97b54109df8a2d9e4
leslab <nyancoro2001@yahoo.co.jp>
2016-10-12 00:08:53 +0900

    edit Tc.java

--- src/main/java/tc/Tc.java
+++ src/main/java/tc/Tc.java
@@ -4,6 +4,11 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.User;
+import twitter4j.Status;
+import java.util.List;
+
+// 2016/10/12
+// Eclipse+mavenでビルド
 
 public class Tc {
     public static void main( String[] args ) throws TwitterException
@@ -16,6 +21,11 @@ public class Tc {
         System.out.println("表示名　　　：" + user.getScreenName());
         System.err.println("フォロー数　：" + user.getFriendsCount());
         System.out.println("フォロワー数：" + user.getFollowersCount());
+        
+        List<Status> statuses = twitter.getHomeTimeline();
+        for (Status status : statuses) {
+            System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+        }
 
     }
 }
